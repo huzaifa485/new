d69bd23d85111c4112fb9cef65cdc6638ec83688
huzaifa485 <128064551+huzaifa485@users.noreply.github.com>
2023-03-31 10:32:41 -0700

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,52 @@
+alert("Ramzan Mubarak")
+
+console.log("ramzan")
+var title = "ramzan"
+
+console.log(title)
+
+var x = 3;
+var y = x + 3;
+
+console.log(y);
+
+var x = 3;
+var x = 6;
+var x = 5;
+var x = (a + b) - c;
+
+
+console.log(x)
+
+var abc = "asd" //legal
+var ab12 = "asd" //legal
+var 123 = "asd" //illegal
+var 12ab = "asd" //illegal
+var %abc = "asd" //illegal
+var var = "asd" //illegal
+var $abc = "asd" //legal
+
+
+var a = 9;
+var x = a++;
+var x = +a;
+var x = --a;
+var x = a--;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+var firstname = prompt("Enter first name");
+var lastname = prompt("Enter last name");
+var city = prompt("enter city" , "karachi");
+
+
+
+
+
+
